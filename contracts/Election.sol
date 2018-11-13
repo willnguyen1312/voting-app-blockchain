8514c805679329982bc2460677d6b43188bfb4bb
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-11-13 14:40:52 +0700

    Update contract to initialize some candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
     // Constructor
     function Election() public {
-        candidate = "Nam Nguyen 1";
+        addCandidate("First");
+        addCandidate("Second");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
